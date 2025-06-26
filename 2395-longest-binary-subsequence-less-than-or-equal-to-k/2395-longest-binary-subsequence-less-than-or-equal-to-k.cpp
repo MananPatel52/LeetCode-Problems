44d6447d4518b6e30a4ceb18916ac83b26e6db55
Manan Kumar <140254298+MananPatel52@users.noreply.github.com>
2025-06-26 22:42:23 +0530

    Time: 0 ms (100%), Space: 9.6 MB (50.79%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -4,26 +4,97 @@ public:
         int zeros = count(s.begin(), s.end(), '0');
         int max_len = 0;
         int temp = k;
-        
-        while (temp > 0) {
-            temp >>= 1;
+        while(temp>0){
+            temp>>=1;
             max_len++;
         }
-        if (max_len == 0) max_len = 1; // Handle k=0 case
-        
-        if (max_len >= s.size()) {
+        if(max_len == 0) max_len = 1;
+        if(max_len >= s.size()){
             return s.size();
         }
-        
-        string candidate = s.substr(s.size() - max_len, max_len);
+        string valid = s.substr(s.size()-max_len, max_len);
+        int zeros_in = count(valid.begin(), valid.end(), '0');
         int num = 0;
-        int zeros_in_candidate = count(candidate.begin(), candidate.end(), '0');
-        for (char c : candidate) {
-            num = stoi(candidate, nullptr, 2);
-            if (num > k) {
-                return zeros + (max_len - 1) - zeros_in_candidate;
+        for(char c:valid){
+            num = stoi(valid, nullptr, 2);
+            if(num>k){
+                return zeros + (max_len - 1) - zeros_in;
             }
         }
-        return zeros + max_len - zeros_in_candidate;
+        return zeros + max_len - zeros_in;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int longestSubsequence(string s, int k) {
+//         int zeros = count(s.begin(), s.end(), '0');
+//         int max_len = 0;
+//         int temp = k;
+        
+//         while (temp > 0) {
+//             temp >>= 1;
+//             max_len++;
+//         }
+//         if (max_len == 0) max_len = 1; // Handle k=0 case
+        
+//         if (max_len >= s.size()) {
+//             return s.size();
+//         }
+        
+//         string candidate = s.substr(s.size() - max_len, max_len);
+//         int num = 0;
+//         int zeros_in_candidate = count(candidate.begin(), candidate.end(), '0');
+//         for (char c : candidate) {
+//             num = stoi(candidate, nullptr, 2);
+//             if (num > k) {
+//                 return zeros + (max_len - 1) - zeros_in_candidate;
+//             }
+//         }
+//         return zeros + max_len - zeros_in_candidate;
+//     }
+// };
