9381973fe72cddac53ba7488e7585c5c0eb2926e
Manan Kumar <140254298+MananPatel52@users.noreply.github.com>
2025-07-06 18:37:37 +0530

    Time: 267 ms (15.45%), Space: 23.9 MB (19.33%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,27 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int max_diameter = 0;
-
-    int height(TreeNode* node){
-        if(!node) return 0;
-        int left = height(node->left);
-        int right = height(node->right);
-        max_diameter = max(max_diameter, left+right);
-        return max(left, right) + 1;
+    int height(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        return (1 + max(height(root->left), height(root->right)));
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        height(root);
-        return max_diameter;
+        if (root == NULL) {
+            return 0;
+        }
+        int option1 = diameterOfBinaryTree(root->left);
+        int option2 = diameterOfBinaryTree(root->right);
+        int option3 = height(root->left) + height(root->right);
+        int diameter = max(option1, max(option2, option3));
+        return diameter;
     }
 };
