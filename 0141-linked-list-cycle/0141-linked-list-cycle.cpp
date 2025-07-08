dc888a0bae5d6317a52ad7b8c22568af37713500
Manan Kumar <140254298+MananPatel52@users.noreply.github.com>
2025-07-08 21:49:27 +0530

    Time: 8 ms (81.22%), Space: 12 MB (23.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,33 +9,20 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*, bool> mp;
-        ListNode* temp = head;
-
-        while(temp!=NULL){
-            if(mp.find(temp) != mp.end()){
+        ListNode* slow  = head;
+        ListNode* fast = head;
+        while(fast!=NULL){
+            fast = fast->next;
+            if(fast!=NULL){
+                fast = fast->next;
+                slow = slow->next;
+            }
+            if(fast==slow){
                 return true;
             }
-            else{
-                mp[temp] = true;
-            }
-            temp = temp->next;
+
         }
         return false;
-
-        // map<ListNode*, bool>table;
-
-        // ListNode* temp = head;
-        // while(temp != NULL){
-        //     if(table[temp] == false){
-        //         table[temp] = true;
-        //     }
-        //     else{
-        //         return true;
-        //     }
-        //     temp = temp->next;
-        // }
-        // return false;
     }
 };
 
@@ -43,17 +30,3 @@ public:
 
 
 
-//         ListNode* slow  = head;
-//         ListNode* fast = head;
-//         while(fast!=NULL){
-//             fast = fast->next;
-//             if(fast!=NULL){
-//                 fast = fast->next;
-//                 slow = slow->next;
-//             }
-//             if(fast==slow){
-//                 return true;
-//             }
-
-//         }
-//         return false;
